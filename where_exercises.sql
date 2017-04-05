83febbf3680362ac994074b307235db249f1fd8e
Joshua Williams <joshuamw322@gmail.com>
2017-04-05 11:52:05 -0500

    more code refactored

--- where_exercises.sql
+++ where_exercises.sql
@@ -14,15 +14,11 @@ SELECT first_name, last_name
 FROM employees
 WHERE last_name LIKE 'E%' AND last_name LIKE '%E';
 
--- Employees hired in the 90s — 
+-- Employees hired in the 90s and born on Christmas— 
 SELECT hire_date, last_name
 FROM employees
-WHERE (hire_date BETWEEN '1990-01-01 00:00:00' AND '1999-12-31 23:59:59');
-
--- Employees born on Christmas — 
-SELECT birth_date, last_name
-FROM employees
-WHERE birth_date LIKE '%-12-25%';
+WHERE (hire_date BETWEEN '1990-01-01 00:00:00' AND '1999-12-31 23:59:59') 
+AND birth_date LIKE '%-12-25%';
 
 -- Employees with a 'q' in their last name and not 'qu' — 
 SELECT first_name, last_name
