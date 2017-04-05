41a911c8ab32576ce768a86260dd42097acf8ea2
Joshua Williams <joshuamw322@gmail.com>
2017-04-05 12:36:27 -0500

    added ORDER BY wand changed order from asc to descesc on queries

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -3,7 +3,7 @@ SELECT first_name, last_name
 FROM employees
 WHERE (first_name = 'Irena' OR first_name ='Vidya' OR first_name = 'Maya')
 AND gender = 'M'
-ORDER BY last_name ASC, first_name ASC;
+ORDER BY last_name DESC, first_name DESC;
 
 -- Employees whose last name starts OR ends with 'E' — 
 SELECT first_name, last_name
@@ -14,7 +14,7 @@ WHERE last_name LIKE 'E%' OR last_name LIKE '%E';
 SELECT first_name, last_name
 FROM employees
 WHERE last_name LIKE 'E%' AND last_name LIKE '%E'
-ORDER BY emp_no;
+ORDER BY emp_no DESC;
 
 
 -- Employees hired in the 90s and born on Christmas— 
