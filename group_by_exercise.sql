847ca79987af627b408407f6393dd0e719c81772
Joshua Williams <joshuamw322@gmail.com>
2017-04-05 14:27:10 -0500

    using GROUP BY to sort through employees table

--- group_by_exercise.sql
+++ group_by_exercise.sql
@@ -3,7 +3,6 @@ SELECT DISTINCT title
 FROM titles
 ORDER BY title;
 
-
 -- Employees whose last name starts OR ends with 'E' — 
 SELECT first_name, last_name
 FROM employees
@@ -15,15 +14,8 @@ FROM employees
 WHERE last_name LIKE 'E%' AND last_name LIKE '%E'
 GROUP BY first_name, last_name;
 
-
--- Employees hired in the 90s and born on Christmas— 
-SELECT hire_date, first_name, last_name
-FROM employees
-WHERE (hire_date BETWEEN '1990-01-01 00:00:00' AND '1999-12-31 23:59:59') 
-AND birth_date LIKE '%-12-25%'
-ORDER BY hire_date DESC, birth_date DESC;
-
 -- Employees with a 'q' in their last name and not 'qu' — 
-SELECT first_name, last_name
+SELECT last_name
 FROM employees
-WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%';
+WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%'
+GROUP BY last_name;
