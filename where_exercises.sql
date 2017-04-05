c3b86fb979ab5f67c1ca4ac0a6806331c05b5a5a
Joshua Williams <joshuamw322@gmail.com>
2017-04-05 11:17:14 -0500

    refactored code to use OR and LIKE conditionals

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,24 +1,25 @@
--- Employees with first names 'Irena', 'Vidya', or 'Maya' — 709 rows (Hint: Use IN).
+-- Employees with first names 'Irena', 'Vidya', or 'Maya' and male
 SELECT first_name, last_name
 FROM employees
-WHERE first_name = 'Irena' OR first_name ='Vidya' OR first_name = 'Maya';
+WHERE (first_name = 'Irena' OR first_name ='Vidya' OR first_name = 'Maya')
+AND gender = 'M';
 
--- Employees whose last name starts with 'E' — 7,330 rows.
+-- Employees whose last name starts OR ends with 'E' — 
 SELECT first_name, last_name
 FROM employees
-WHERE last_name LIKE 'E%';
+WHERE last_name LIKE 'E%' OR last_name LIKE '%E';
 
--- Employees hired in the 90s — 135,214 rows.
+-- Employees hired in the 90s — 
 SELECT hire_date, last_name
 FROM employees
 WHERE (hire_date BETWEEN '1990-01-01 00:00:00' AND '1999-12-31 23:59:59');
 
--- Employees born on Christmas — 842 rows.
+-- Employees born on Christmas — 
 SELECT birth_date, last_name
 FROM employees
 WHERE birth_date LIKE '%-12-25%';
 
--- Employees with a 'q' in their last name — 1,873 rows.
+-- Employees with a 'q' in their last name — 
 SELECT first_name, last_name
 FROM employees
 WHERE last_name LIKE '%q%';
