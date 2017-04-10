fef499d5e9c102be8a4699ced8f36609f425e5b6
Joshua Williams <joshuamw322@gmail.com>
2017-04-10 09:54:34 -0500

    exercise working with subquerires

--- subqueries.sql
+++ subqueries.sql
@@ -1,5 +1,5 @@
 -- Find all the employees with the same hire date as employee 101010 using a sub-query.
-
+-- 69 rows
 SELECT *
 FROM employees
 WHERE hire_date IN (
@@ -8,7 +8,7 @@ WHERE hire_date IN (
 	WHERE emp_no = 101010);
 
 -- Find all the titles held by all employees with the first name Aamod.
-
+-- 314 rows
 SELECT title
 FROM titles 
 WHERE emp_no IN (
@@ -17,7 +17,7 @@ WHERE emp_no IN (
 	WHERE first_name = 'Aamod');
 
 -- Find all the department managers that are female.
-
+-- 13 rows
 SELECT emp_no
 FROM dept_manager
 WHERE emp_no IN (
@@ -26,6 +26,7 @@ WHERE emp_no IN (
 	WHERE gender = 'F');
 
 -- BONUS Find all the department names that have female managers.
+-- 7 rows
 SELECT dept_name
 FROM departments
 WHERE dept_no IN (
